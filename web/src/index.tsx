e98e1a2c4a7688bf7e89d4756f09748b1003b088
Alexey Demedetskiy <alexey.demedetskiy@sigma.software>
2018-12-09 01:02:44 +0200

    Bootsrap react + typescript

--- web/src/index.tsx
+++ web/src/index.tsx
@@ -1,15 +1,12 @@
-// tslint:disable-next-line:no-import-side-effect
-import 'tslib';
-import * as React from 'react';
-import { render } from 'react-dom';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import * as serviceWorker from './serviceWorker';
 
-const styles: React.CSSProperties = {
-  fontFamily: 'sans-serif',
-  textAlign: 'center',
-};
+ReactDOM.render(<App />, document.getElementById('root'));
 
-const Root = () => (
-  <div style={styles}/>
-);
-
-render(<Root />, document.getElementById('root'));
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: http://bit.ly/CRA-PWA
+serviceWorker.unregister();
